0361d758fcaaefc1b8c323e838bb29eb8b4e65c6
Buzek Jonatan <buzek.jonatan@gmail.com>
2019-12-08 19:18:06 +0100

    change template css link

--- view/template.php
+++ view/template.php
@@ -15,7 +15,7 @@
     <a href="<?php $_SERVER['QUERY_STRING']?>?locale=fr_FR">FR</a>
     <a href="<?php $_SERVER['QUERY_STRING']?>?locale=en_US">US</a>
     <title>MyOwnDictionary</title>
-    <link href="/public/style.css" rel="stylesheet" />
+    <link href="public/style.css" rel="stylesheet" />
   </head>
   <body>
     <h1>MyOwnDictionary</h1>
